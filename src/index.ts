f1254a9b2e15cb5339fdb0b2e006ef3b10a70515
siddhida <jhasiddhida@gmail.com>
2023-09-22 17:42:13 +0530

    index chg

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,81 @@
-import 'reflect-metadata';
-import { container } from "tsyringe";
-import express, { urlencoded, Express } from "express";
-import cors from 'cors';
-import { createConnection, Connection } from "typeorm";
-import { useExpressServer } from 'routing-controllers';
-import { TaskController } from './controllers/TaskController';
-import { UserController } from './controllers/UserController';
-import { BookController } from './controllers/BookController';
-import { PalController } from './controllers/PalController';
-import { HomeController } from './controllers/HomeController';
+import express from "express";
 
-const { config } = require("./../ormconfig");
-const PORT = 3005;
-const app: Express = express();
-// let containers = [UserController, ProductController];
-//===============================================
-// app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
-// app.use(bodyParser.json());
-// app.use(httpContext.middleware);
+const app = express();
+const port = 9000;
+app.use("/", (req, res) => {
+  res.json({ message: "Hello From Express App" });
+});
 
-// console.log("ObservabilityMiddleware ==== ", ObservabilityMiddleware);
-// app.post('/', (req, res) => {
-  //   console.log("Express ==== ", req.body); // Log the request body
-  //   // Your code to handle the request
-  //   return res.send(req.body);
-  // });
-//=====================================
+app.listen(9000, () => {
+  console.log(`Starting Server on Port ${port}`);
+});
 
-createConnection(config)
-.then(async (connection) => {
-  console.log("Connected to the database");
-  container.resolve(TaskController);
-  container.resolve(UserController);
-  container.resolve(BookController);
-  container.resolve(PalController);
-  let containers = [HomeController];
-  // let containers = [HomeController,UserController, BookController, PalController, TaskController];
- //================================================================ 
-  // const app = createExpressServer({
-  //   controllers: [UserController, ProductController], // Add your controllers here
-  // });
+// import 'reflect-metadata';
+// import { container } from "tsyringe";
+// import express, { urlencoded, Express } from "express";
+// import cors from 'cors';
+// import { createConnection, Connection } from "typeorm";
+// import { useExpressServer } from 'routing-controllers';
+// import { TaskController } from './controllers/TaskController';
+// import { UserController } from './controllers/UserController';
+// import { BookController } from './controllers/BookController';
+// import { PalController } from './controllers/PalController';
+// import { HomeController } from './controllers/HomeController';
 
-  // app.use(express.json());
+// const { config } = require("./../ormconfig");
+// const PORT = 3005;
+// const app: Express = express();
 
-  // app.listen(PORT, () => {
-  //   console.log(`Server is running on PORT ${PORT}`);
-  // });
+// app.use(cors());
 
- //================================================================ 
-useExpressServer(app,{
-  // middlewares,
+// createConnection(config)
+// .then(async (connection) => {
+//   console.log("Connected to the database ==== ");
+//   container.resolve(TaskController);
+//   container.resolve(UserController);
+//   container.resolve(BookController);
+//   container.resolve(PalController);
+//   let containers = [HomeController];
+//   // let containers = [HomeController,UserController, BookController, PalController, TaskController];
+//  //================================================================ 
+//   // const app = createExpressServer({
+//   //   controllers: [UserController, ProductController], // Add your controllers here
+//   // });
 
-  controllers: containers,
-  // controllers: [UserController],
-  // defaultErrorHandler:false,
-  // validation: {
-  //   whitelist: true,
-  //   forbidNonWhitelisted:true,
-  //   forbidUnknownValues:true,
-  // }
-}).listen(PORT, ()=>{
-  console.log(`Server is running on PORT ${PORT}`);
-})
- //================================================================ 
+//   // app.use(express.json());
 
-//  useExpressServer(app, {
-//   controllers: [UserController], // we specify controllers we want to use
-//   defaultErrorHandler: false
+//   // app.listen(PORT, () => {
+//   //   console.log(`Server is running on PORT ${PORT}`);
+//   // });
+
+//  //================================================================ 
+// useExpressServer(app,{
+//   // middlewares,
+
+//   controllers: containers,
+//   // controllers: [UserController],
+//   // defaultErrorHandler:false,
+//   // validation: {
+//   //   whitelist: true,
+//   //   forbidNonWhitelisted:true,
+//   //   forbidUnknownValues:true,
+//   // }
 // }).listen(PORT, ()=>{
 //   console.log(`Server is running on PORT ${PORT}`);
 // })
-//================================================================ 
+//  //================================================================ 
+
+// //  useExpressServer(app, {
+// //   controllers: [UserController], // we specify controllers we want to use
+// //   defaultErrorHandler: false
+// // }).listen(PORT, ()=>{
+// //   console.log(`Server is running on PORT ${PORT}`);
+// // })
+// //================================================================ 
 
 
-})
-.catch((error) => {
-  console.error("Error connecting to the database:", error);
-});
+// })
+// .catch((error) => {
+//   console.error("Error connecting to the database:", error);
+// });
 
